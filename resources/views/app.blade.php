4cd7d65e88173672f4ce7c05f8fc0b4e8a0fdb5a
Zyrus2k <cataloniazyrus328@gmail.com>
2026-09-16 09:14:28 +0800

    Update React application shell

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title>Deskline | Computer Cafe Station Management</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        @routes
         @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
-        @inertiaHead
+        @vite(['resources/css/app.css', 'resources/js/app.tsx'])
     </head>
-    <body class="font-sans antialiased">
-        @inertia
+    <body>
+        <div id="root"></div>
     </body>
 </html>
